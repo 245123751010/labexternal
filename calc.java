5de898ea27087cf597f7ab4f8fb64b480fb17ca3
Raviteja-88 <245123751060@mvsrec.edu.in>
2026-06-25 10:12:48 +0530

    added 2 files

---
+++ calc.java
@@ -0,0 +1,10 @@
+
+public class Calculator {
+    public static void main(String[] args) {
+        int a = 20, b = 4;
+        System.out.println("ADD: " + (a+b));
+        System.out.println("SUB: " + (a-b));
+        System.out.println("MUL: " + (a*b));
+        System.out.println("DIV: " + (a/b));
+    }
+}
---
+++ calc.py
@@ -0,0 +1,6 @@
+a, b = 20, 4
+print(f"ADD: {a+b}")
+print(f"SUB: {a-b}")
+print(f"MUL: {a*b}")
+print(f"DIV: {a/b}")
+
