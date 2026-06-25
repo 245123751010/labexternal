a5612c8df5079ba7f3ac880b7cc0f0447a0a8764
Raviteja-88 <245123751060@mvsrec.edu.in>
2026-06-25 10:21:24 +0530

    added calc.java calc.py

--- calc.java
+++ calc.java
@@ -1,7 +1,8 @@
 
-public class Calculator {
+public class calc {
     public static void main(String[] args) {
-        int a = 20, b = 4;
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
         System.out.println("ADD: " + (a+b));
         System.out.println("SUB: " + (a-b));
         System.out.println("MUL: " + (a*b));
--- calc.py
+++ calc.py
@@ -1,4 +1,6 @@
-a, b = 20, 4
+import sys
+a = int(sys.argv[1]) 
+b = int(sys.argv[2])
 print(f"ADD: {a+b}")
 print(f"SUB: {a-b}")
 print(f"MUL: {a*b}")
